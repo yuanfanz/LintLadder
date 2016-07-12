aa795a62f035e8d97f62f9f630de9d19b7e49e9b
yuanfanz <yuanfanz@uci.edu>
2016-07-11 17:35:52 -0700

    .

--- src/Tree/Basic/BalancedTree.java
+++ src/Tree/Basic/BalancedTree.java
@@ -18,19 +18,19 @@ public class BalancedTree {
      */
     public boolean isBalanced(TreeNode root) {
         // write your code here
-        ResultType result = helper(root);
+        ResultType2 result = helper(root);
         return result.isBalanced;
     }
-    private ResultType helper(TreeNode root){
+    private ResultType2 helper(TreeNode root){
         if (root == null){
-            return new ResultType(true, 0);
+            return new ResultType2(true, 0);
         }
         //divide
-        ResultType left = helper(root.left);
-        ResultType right = helper(root.right);
+        ResultType2 left = helper(root.left);
+        ResultType2 right = helper(root.right);
         //conquer
         if(left.isBalanced && right.isBalanced && Math.abs(left.maxDepth - right.maxDepth) <= 1){
-            return new ResultType(true, Math.max(left.maxDepth, right.maxDepth) + 1);
-        } else return new ResultType(false, -1);
+            return new ResultType2(true, Math.max(left.maxDepth, right.maxDepth) + 1);
+        } else return new ResultType2(false, -1);
     }
 }
---
+++ src/Tree/Basic/ValidBinarySearchTree.java
@@ -0,0 +1,41 @@
+package Tree.Basic;
+
+/**
+ * Created by Frank on 16/7/11.
+ */
+class ResultType2 {
+    boolean isBST;
+    int minValue, maxValue;
+    ResultType2(boolean isBST, int maxValue, int minValue){
+        this.isBST = isBST;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+}
+public class ValidBinarySearchTree {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        // write your code here
+        ResultType2 r = helper(root);
+        return r.isBST;
+    }
+    private ResultType2 helper(TreeNode root){
+        if(root == null){
+            return new ResultType2(true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        ResultType2 left = helper(root.left);
+        ResultType2 right = helper(root.right);
+        if(!left.isBST || !right.isBST){
+            return new ResultType2(false, 0, 0);
+        }
+        if(root.left != null && left.maxValue >= root.val ||
+                root.right != null && right.minValue <= root.val){
+            return new ResultType2(false, 0, 0);
+        }
+        return new ResultType2(true, Math.max(root.val, right.maxValue),
+                                     Math.min(root.val, left.minValue));
+    }
+}

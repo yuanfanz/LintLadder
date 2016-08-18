3cdfbbe72805ff22b70ae2595ab66dc5e20d4b41
yuanfanz <yuanfanz@uci.edu>
2016-08-17 20:43:18 -0700

    .

---
+++ src/Array/SubarraySum.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Frank on 16/8/15.
+ */
+public class SubarraySum {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number
+     *          and the index of the last number
+     */
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        // write your code here
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(nums == null || nums.length == 0){
+            return result;
+        }
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, -1);
+        for(int i = 0; i < nums.length; ++i){
+            sum += nums[i];
+            if(map.containsKey(sum)){
+                result.add(map.get(sum) + 1);
+                result.add(i);
+                return result;
+            }
+            map.put(sum, i);
+        }
+        return result;
+    }
+}
--- src/LinkedList/Advanced/CopyListWithRandomPointer.java
+++ src/LinkedList/Advanced/CopyListWithRandomPointer.java
@@ -95,4 +95,3 @@ public class CopyListWithRandomPointer {
         return dummy;
     }
 }
-}

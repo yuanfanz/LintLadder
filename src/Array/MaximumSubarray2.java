86af701031339369c9a08e992da684d1d56899d0
yuanfanz <yuanfanz@uci.edu>
2016-08-18 12:16:18 -0700

    .

---
+++ src/Array/MaximumSubarray2.java
@@ -0,0 +1,44 @@
+package Array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Frank on 16/8/15.
+ */
+public class MaximumSubarray2 {
+    /**
+     * @param nums: A list of integers
+     * @return: An integer denotes the sum of max two non-overlapping subarrays
+     */
+    public int maxTwoSubArrays(ArrayList<Integer> nums) {
+        // write your code
+        if(nums == null || nums.isEmpty()){
+            return 0;
+        }
+        int minSum = 0;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int[] left = new int[nums.size()];
+        int[] right = new int[nums.size()];
+        for(int i = 0; i < nums.size(); ++i){
+            sum += nums.get(i);
+            max = Math.max(max, sum - minSum);
+            minSum = Math.min(minSum, sum);
+            left[i] = max;
+        }
+        minSum = 0;
+        sum = 0;
+        max = Integer.MIN_VALUE;
+        for(int i = nums.size() - 1; i >= 0; --i){
+            sum += nums.get(i);
+            max = Math.max(max, sum - minSum);
+            minSum = Math.min(minSum, sum);
+            right[i] = max;
+        }
+        max = Integer.MIN_VALUE;
+        for(int i = 0; i + 1 < nums.size(); ++i){
+            max = Math.max(max, left[i] + right[i + 1]);
+        }
+        return max;
+    }
+}
---
+++ src/Array/MinimumSubarray.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Frank on 16/8/15.
+ */
+public class MinimumSubarray {
+    /**
+     * @param nums: a list of integers
+     * @return: A integer indicate the sum of minimum subarray
+     */
+    public int minSubArray(ArrayList<Integer> nums) {
+        // write your code
+        if(nums == null || nums.isEmpty()){
+            return 0;
+        }
+        int maxSum = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.size(); ++i){
+            sum += nums.get(i);
+            min = Math.min(min, sum - maxSum);
+            maxSum = Math.max(maxSum, sum);
+        }
+        return min;
+    }
+}

dcc84707e8100a61f63fc887c1b4e21e60f8f965
yuanfanz <yuanfanz@uci.edu>
2016-12-21 22:23:13 -0800

    .

--- src/Tree/Basic/BalancedTree.java
+++ src/Tree/Basic/BalancedTree.java
@@ -1,36 +1,26 @@
-//package Tree.Basic;
-//
-///**
-// * Created by yuanf on 2016/7/8.
-// */
-//class ResultType {
-//    public boolean isBalanced;
-//    public int maxDepth;
-//    public ResultType(boolean isBalanced, int maxDepth) {
-//        this.isBalanced = isBalanced;
-//        this.maxDepth = maxDepth;
-//    }
-//}
-//public class BalancedTree {
-//    /**
-//     * @param root: The root of binary tree.
-//     * @return: True if this Binary tree is Balanced, or false.
-//     */
-//    public boolean isBalanced(TreeNode root) {
-//        // write your code here
-//        ResultType2 result = helper(root);
-//        return result.isBalanced;
-//    }
-//    private ResultType2 helper(TreeNode root){
-//        if (root == null){
-//            return new ResultType2(true, 0);
-//        }
-//        //divide
-//        ResultType2 left = helper(root.left);
-//        ResultType2 right = helper(root.right);
-//        //conquer
-//        if(left.isBalanced && right.isBalanced && Math.abs(left.maxDepth - right.maxDepth) <= 1){
-//            return new ResultType2(true, Math.max(left.maxDepth, right.maxDepth) + 1);
-//        } else return new ResultType2(false, -1);
-//    }
-//}
+package Tree.Basic;
+
+/**
+ * Created by yuanf on 2016/7/8.
+ */
+public class BalancedTree {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        return (getDepth(root) != -1);
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+
+        if (left == -1 || right == -1) return -1;
+        if (Math.abs(left - right) > 1) return -1;
+
+        return Math.max(left, right) + 1;
+    }
+}
